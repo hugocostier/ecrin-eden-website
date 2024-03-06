4a91d5ac3565168b8fa0642db101d25b388e3c2b
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-06 19:11:17 +0100

    Refactor page imports and organize into sections

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,9 +1,34 @@
-export { ErrorPage } from './Error.page'
+// Store front pages
 export { CertificationPage } from './store-front/Certification.page'
 export { ContactPage } from './store-front/Contact.page'
 export { GiftCardsPage } from './store-front/GiftCards.page'
 export { HomePage } from './store-front/Home.page'
-export { LoginRegisterPage } from './store-front/Login.page'
 export { PricesPage } from './store-front/Prices.page'
 export { ServicesPage } from './store-front/Services.page'
 
+// User dashboard pages
+export { AccountPage } from './private-dashboard/Account.page'
+export { SettingsPage } from './private-dashboard/Settings.page'
+export { MyDashboard } from './private-dashboard/client-space/Dashboard.page'
+export { MyPreferences } from './private-dashboard/client-space/Preferences.page'
+export { MyAppointments } from './private-dashboard/client-space/appointments/Appointments.page'
+// export { UpdateAppointment } from './private-dashboard/client-space/appointments/UpdateAppointment.page'
+
+// Admin dashboard pages
+export { AdminDashboard } from './private-dashboard/admin-space/Dashboard.page'
+export { AddAppointment } from './private-dashboard/admin-space/appointments/AddAppointment.page'
+export { AdminAppointments } from './private-dashboard/admin-space/appointments/Appointments.page'
+export { SeeAppointment } from './private-dashboard/admin-space/appointments/SeeAppointment.page'
+export { UpdateAppointment } from './private-dashboard/admin-space/appointments/UpdateAppointment.page'
+export { AddClient } from './private-dashboard/admin-space/clients/AddClient.page'
+export { AdminClients } from './private-dashboard/admin-space/clients/Clients.page'
+export { SeeClient } from './private-dashboard/admin-space/clients/SeeClient.page'
+export { UpdateClient } from './private-dashboard/admin-space/clients/UpdateClient.page'
+export { AdminContent } from './private-dashboard/admin-space/content/Content.page'
+export { UpdateContent } from './private-dashboard/admin-space/content/UpdateContent.page'
+
+// Authentication pages
+export { LoginRegisterPage } from './Login.page'
+
+// Error pages
+export { ErrorPage } from './Error.page'
