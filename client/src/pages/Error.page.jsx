7f4c9960cdc3794aafc890c81a8ca81f499469a5
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-06 19:11:52 +0100

    Fix useRouteError hook in ErrorPage

--- client/src/pages/Error.page.jsx
+++ client/src/pages/Error.page.jsx
@@ -1,7 +1,7 @@
 import { useRouteError } from "react-router-dom";
 
 export const ErrorPage = () => {
-    const error = useRouteError() 
+    const error = useRouteError()
     console.error(error)
 
     return (
