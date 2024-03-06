c1a2909e21f8cb18c92b3753774956a1419929cf
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-06 19:10:14 +0100

    Add Account and Settings pages to private dashboard

---
+++ client/src/pages/private-dashboard/Account.page.jsx
@@ -0,0 +1,8 @@
+export const AccountPage = () => {
+    return (
+        <div>
+            <h1>My Account</h1>
+            <p>Manage your account here.</p>
+        </div>
+    )
+}
---
+++ client/src/pages/private-dashboard/Settings.page.jsx
@@ -0,0 +1,8 @@
+export const SettingsPage = () => {
+    return (
+        <div>
+            <h1>My Settings</h1>
+            <p>Manage your settings here.</p>
+        </div>
+    )
+}
