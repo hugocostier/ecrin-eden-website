1fccaddc2698c6a5931ff30ac1d427f7a941c20a
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-06 17:08:30 +0100

    Refactor routing and add authentication support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,103 +1,120 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import { certificationLoader, contactLoader, giftCardLoader, homeLoader, priceLoader, serviceLoader } from './data'
-import { StoreRoot } from './layouts'
-import { CertificationPage, ContactPage, ErrorPage, GiftCardsPage, HomePage, PricesPage, ServicesPage } from './pages'
+import { AuthProvider } from './hooks/useAuth'
+
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements
+} from 'react-router-dom'
+
+import {
+    certificationLoader,
+    contactLoader,
+    giftCardLoader,
+    homeLoader,
+    priceLoader,
+    serviceLoader
+} from './data'
+
+import {
+    ClientRoot,
+    StoreRoot
+} from './layouts'
+
+import {
+    CertificationPage,
+    ContactPage,
+    ErrorPage,
+    GiftCardsPage,
+    HomePage,
+    LoginRegisterPage,
+    PricesPage,
+    ServicesPage
+} from './pages'
+
+import { PrivateRoute } from './components/PrivateRoute'
 
 const Router = createBrowserRouter(
     createRoutesFromElements(
-        <Route
-            path='/'
-            element={<StoreRoot />}
-            errorElement={<ErrorPage />}
-        >
+        <Route element={<AuthProvider />} >
             <Route
-                index={true}
-                element={<HomePage />}
-                loader={homeLoader}
-            />
+                path='/'
+                element={<StoreRoot />}
+                errorElement={<ErrorPage />}
+            >
+                <Route
+                    index={true}
+                    element={<HomePage />}
+                    loader={homeLoader}
+                />
+
+                <Route
+                    path='services'
+                    element={<ServicesPage />}
+                    loader={serviceLoader}
+                />
+
+                <Route
+                    path='prices'
+                    element={<PricesPage />}
+                    loader={priceLoader}
+                />
+
+                <Route
+                    path='gift-cards'
+                    element={<GiftCardsPage />}
+                    loader={giftCardLoader}
+                />
+
+                <Route
+                    path='certification'
+                    element={<CertificationPage />}
+                    loader={certificationLoader}
+                />
+
+                <Route
+                    path='contact'
+                    element={<ContactPage />}
+                    loader={contactLoader}
+                />
+            </Route>
 
             <Route
-                path='services'
-                element={<ServicesPage />}
-                loader={serviceLoader}
+                path='/login'
+                element={<LoginRegisterPage />}
+                errorElement={<ErrorPage />}
             />
 
-            <Route
-                path='prices'
-                element={<PricesPage />}
-                loader={priceLoader}
-            />
-
-            <Route 
-                path='gift-cards'
-                element={<GiftCardsPage />}
-                loader={giftCardLoader}
-            />
-
-            <Route
-                path='certification'
-                element={<CertificationPage />}
-                loader={certificationLoader}
-            />
-
-            <Route
-                path='contact'
-                element={<ContactPage />}
-                loader={contactLoader}
-            />
+            <Route element={<PrivateRoute />} >
+                <Route
+                    path='/user'
+                    element={<ClientRoot />}
+                    errorElement={<ErrorPage />}
+                >
+                    <Route
+                        index={true}
+                        element={<ContactPage />}
+                        loader={contactLoader}
+                    />
+                </Route>
+            </Route>
         </Route>
     )
 )
-    // [
-    // {
-    //     path: '/', 
-    //     element: <StoreRoot />,
-    //     errorElement: <ErrorPage />, 
-    //     children: [
-    //         {
-    //             index: true, 
-    //             element: <HomePage />, 
-    //         }, 
-            // {
-            //     path: 'about', 
-            //     element: <About />,
-            // }, 
-            // {
-            //     path: 'pricing', 
-            //     element: <Pricing />, 
-            // }, 
-            // {
-            //     path: 'giftcard', 
-            //     element: <GiftCard />, 
-            // }, 
-            // {
-            //     path: 'certification', 
-            //     element: <Certification />, 
-            // }, 
-            // {
-            //     path: 'contact', 
-            //     element: <Contact />, 
-            // }, 
-            // {
-            //     path: 'login', 
-            //     element: <Login />, 
-            // }, 
-            // {
-            //     path: 'dashboard', 
-            //     element: <DashboardLayout queryClient={queryClient}/>, 
-            //     children: [
-            //         {
-            //             path: 'admin', 
-            //             element: <Admin />, 
-            //         },
-            //     ]
-            // }, 
-//         ] 
-//     }, 
-// ])
 
-export const App = () => { 
+// {
+//     path: 'dashboard', 
+//     element: <DashboardLayout queryClient={queryClient}/>, 
+//     children: [
+//         {
+//             path: 'admin', 
+//             element: <Admin />, 
+//         },
+//     ]
+// }
+
+export const App = () => {
     return (
-        <RouterProvider router={Router} />
+        <RouterProvider router={Router} /> // Add fallback element for when the page is loading
     )
 }
