26b44bc2cda43819bd9fba0e64a7ac2162176ce0
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-16 19:01:01 +0100

    Add dashboard page with user-specific data and styling

--- client/src/pages/private-dashboard/client-space/Dashboard.page.jsx
+++ client/src/pages/private-dashboard/client-space/Dashboard.page.jsx
@@ -1,8 +1,101 @@
+import { useEffect, useState } from 'react'
+import StyledComponents from 'styled-components'
+import image from '../../../assets/images/dashboard.jpg'
+import { Calendar } from '../../../components/private-dashboard/calendar/Calendar'
+import { useUserInfo } from '../../../hooks/useUserInfo.hook'
+import { calculateWeekBounds } from '../../../utils/calculateWeekBounds.util'
+
+const StyledDashboard = StyledComponents.main`
+    display: grid; 
+    grid-template-columns: 3fr 1fr; 
+    grid-template-rows: 200px max-content;
+    gap: 20px;
+    padding: 20px;     
+
+    overflow-y: auto;
+
+    .page-header {
+        grid-column: 1 / 3; 
+    }
+
+    .calendar-container {
+        grid-column: 1 / 2;
+        grid-row: 2 / 3; 
+    }
+
+    aside {
+        grid-column: 2 / 3;
+        grid-row: 2 / 3; 
+
+        img {
+            width: 100%; 
+            height: 100%;
+            object-fit: cover; 
+            object-position: 70% 30%; 
+            border-radius: 10px;
+        }
+    }
+`
+
 export const MyDashboard = () => {
+    const user = useUserInfo();
+    const [count, setCount] = useState(null);
+
+    useEffect(() => {
+        if (user.id) {
+            const weekBounds = calculateWeekBounds(new Date());
+            const firstDayOfWeek = weekBounds.firstDay.toISOString().split('T')[0];
+            const lastDayOfWeek = weekBounds.lastDay.toISOString().split('T')[0];
+
+            const fetchData = async () => {
+                try {
+                    const response = await fetch(`http://localhost:3000/api/v1/appointments/count/${user.id}`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({
+                            startDate: firstDayOfWeek,
+                            endDate: lastDayOfWeek,
+                        }),
+                        credentials: 'include'
+                    });
+
+                    if (!response.ok) {
+                        throw new Error('HTTP Error ! Status: ' + response.status);
+                    }
+
+                    const data = await response.json();
+                    setCount(data.data); // Update count state with fetched data
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                }
+            };
+
+            fetchData();
+        }
+    }, [user.id]); // Fetch data whenever user.id changes
+
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome to your dashboard.</p>
-        </div>
-    )
-}
+        <StyledDashboard>
+            <section className='page-header'>
+                <h2>Bonjour {user.firstName},</h2>
+                {count !== null && count > 0 ?
+                    (
+                        <p>Tu as {count} rendez-vous cette semaine.</p>
+                    )
+                    : count === null ? (
+                        <p>Tu n&apos;as pas de rendez-vous cette semaine.</p>
+                    ) : (
+                        <p>Loading...</p>
+                    )}
+            </section>
+
+            <Calendar />
+
+            <aside>
+                <img src={image} alt='dashboard' />
+            </aside>
+        </StyledDashboard>
+    );
+};
