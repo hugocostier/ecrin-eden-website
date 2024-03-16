1f59e0acc4f4693791df8549bb108d0d22bf4394
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-16 18:52:09 +0100

    Add client repository and update delete user method

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,10 +1,11 @@
 import { User } from '../entities/User.js'
 import { CustomAPIError } from '../errors/custom-errors.js'
+import { ClientRepository } from '../repositories/client.repository.js'
 import { UserRepository } from '../repositories/user.repository.js'
-
 
 export class UserService {
     private _userRepository = UserRepository
+    private _clientRepository = ClientRepository
 
     async getAllUsers() {
         const allUsers = await this._userRepository.find()
@@ -48,10 +49,40 @@ export class UserService {
     }
 
     async deleteUser(id: string) {
-        const user = await this._userRepository.findById(parseInt(id))
+        const user = await this._userRepository.createQueryBuilder('user') 
+            .leftJoinAndSelect('user.client', 'client') 
+            .where('user.id = :id', { id })
+            .getOne()
+
+        console.log('User delete:', user)
 
         if (!user) {
             throw new CustomAPIError(`No user found with id ${id}`, 404)
+        }
+
+        if (user.client) {
+            const clientId = user.client.id 
+
+            if (clientId === undefined) {
+                throw new Error('Client ID is undefined')
+            } 
+
+            const client = await this._clientRepository.createQueryBuilder('client')
+                .leftJoinAndSelect('client.user', 'user')
+                .where('client.id = :id', { id: clientId })
+                .getOne()
+
+            console.log('Client to update:', client)
+
+            if (!client) {
+                throw new CustomAPIError(`No client found with id ${clientId}`, 404)
+            }
+
+            client.user = null 
+
+            const clientUpdated = await this._clientRepository.save(client)
+
+            console.log('Client updated:', clientUpdated)
         }
 
         return await this._userRepository.delete(id)
