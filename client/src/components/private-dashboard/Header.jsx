f8d4eedc2f735a04dfdfa5eb23a4fa22fa930126
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-19 19:34:06 +0100

    Update Header component with new styling, add responsiveness and logout functionality

--- client/src/components/private-dashboard/Header.jsx
+++ client/src/components/private-dashboard/Header.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import StyledComponents from 'styled-components';
 import logo from '../../assets/images/logo.jpg';
+import { useAuth } from '../../hooks/useAuth.hook';
 
 const StyledHeader = StyledComponents.header`
     display: grid;
-    grid-template-columns: auto 1fr auto;
+    grid-template-columns: auto 1fr; 
     justify-content: center;
     align-items: center;
     height: 60px;
@@ -20,7 +21,7 @@ const StyledHeader = StyledComponents.header`
         display: flex;
         justify-content: center;
         align-items: center;
-        margin-left: 32px;
+        margin-left: 5vw;
 
         .logo-nav {
             height: 100%;
@@ -34,43 +35,85 @@ const StyledHeader = StyledComponents.header`
         }
     }
 
-    .title-container {
-        grid-column: 2 / 3;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        
-        h2 {
-            font-size: 2.25rem;
-            margin: 0;
-            line-height: 1;
-        }
-    }
-
     .user-container {
         grid-column: 3 / 4;
         display: flex;
         justify-content: flex-end;
         align-items: center;
+        align-items: center;
+        margin-right: 5vw;
 
         .user {
             height: 50px;
             display: flex;
             justify-content: center;
-            align-items: center;
-            margin-right: 32px;
 
             p {
                 margin: 0;
                 text-transform: capitalize;
             }
+
+            .logout {
+                width: 26px;
+                height: 26px;
+                margin-left: 16px; 
+                color: var(--red-light);
+                cursor: pointer;
+    
+                &:hover {
+                    color: var(--red-dark);
+                }
+            }
         }
+    }
+
+    @media screen and (max-width: 639px) {
+        .title-container {
+            display: none;
+        }
+    }
+
+    @media screen and (min-width: 640px) {
+        grid-template-columns: auto 1fr auto;
+
+        .logo-container {
+            margin-left: 32px; 
+        }
+
+        .title-container {
+            display: flex;
+            grid-column: 2 / 3;
+            justify-content: center;
+            align-items: center;
+            
+            h2 {
+                font-size: 2.25rem;
+                margin: 0;
+                line-height: 1;
+            }
+        }
+
+        .user-container {
+            margin-right: 32px;
+        }
+    }
+
+    @media screen and (min-width: 1024px) {
+
     }
 `
 
 export const Header = ({ user }) => {
+    const auth = useAuth()
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        navigate('/')
+        auth.logOut()
+    }
+
     return (
-        <StyledHeader>
+        <StyledHeader className='user-header'>
             <div className='logo-container'>
                 <Link to={'/'} key={'logo-accueil'} className='logo-nav'>
                     <img src={logo} alt='logo' />
@@ -85,6 +128,12 @@ export const Header = ({ user }) => {
                 <div className='user'>
                     <img id='user-picture' src={user.profilePicture ? user.profilePicture : 'src/assets/images/default-profile-picture.png'} alt='profile' />
                     <p>{user.firstName && user.lastName ? user.firstName + ' ' + user.lastName : 'John Doe'}</p>
+                    <button
+                        onClick={() => handleLogout()}
+                        className='logout'
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M4 18H6V20H18V4H6V6H4V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V18ZM6 11H13V13H6V16L1 12L6 8V11Z"></path></svg>
+                    </button>
                 </div>
             </div>
         </StyledHeader>
