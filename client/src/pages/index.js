a9c2bfb4edd3bd8d7c56e5793e0ab1a74c5f853c
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-20 23:32:51 +0100

    Update page imports in index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -8,14 +8,13 @@ export { ServicesPage } from './store-front/Services.page'
 
 // User dashboard pages
 export { AccountPage } from './private-dashboard/Account.page'
-export { SettingsPage } from './private-dashboard/Settings.page'
+export { MyAppointments } from './private-dashboard/client-space/appointments/Appointments.page'
 export { MyDashboard } from './private-dashboard/client-space/Dashboard.page'
 export { MyPreferences } from './private-dashboard/client-space/Preferences.page'
-export { MyAppointments } from './private-dashboard/client-space/appointments/Appointments.page'
+export { SettingsPage } from './private-dashboard/Settings.page'
 // export { UpdateAppointment } from './private-dashboard/client-space/appointments/UpdateAppointment.page'
 
 // Admin dashboard pages
-export { AdminDashboard } from './private-dashboard/admin-space/Dashboard.page'
 export { AddAppointment } from './private-dashboard/admin-space/appointments/AddAppointment.page'
 export { AdminAppointments } from './private-dashboard/admin-space/appointments/Appointments.page'
 export { SeeAppointment } from './private-dashboard/admin-space/appointments/SeeAppointment.page'
@@ -26,9 +25,12 @@ export { SeeClient } from './private-dashboard/admin-space/clients/SeeClient.pag
 export { UpdateClient } from './private-dashboard/admin-space/clients/UpdateClient.page'
 export { AdminContent } from './private-dashboard/admin-space/content/Content.page'
 export { UpdateContent } from './private-dashboard/admin-space/content/UpdateContent.page'
+export { AdminDashboard } from './private-dashboard/admin-space/Dashboard.page'
 
 // Authentication pages
 export { LoginRegisterPage } from './Login.page'
+export { RecoverPassword } from './RecoverPassword.page'
+export { ResetPassword } from './ResetPassword.page'
 
 // Error pages
 export { ErrorPage } from './Error.page'
