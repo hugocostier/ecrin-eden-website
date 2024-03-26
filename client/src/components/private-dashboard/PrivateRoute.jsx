beff92a2aa9d442e9865d71ea30d9d13ea4d7c69
Hugo Costier <hugo.costier.development@gmail.com>
2024-03-26 17:25:18 +0100

    Refactor PrivateRoute component to handle loading state

--- client/src/components/private-dashboard/PrivateRoute.jsx
+++ client/src/components/private-dashboard/PrivateRoute.jsx
@@ -1,14 +1,21 @@
 import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../../hooks/useAuth.hook'
 
 export const PrivateRoute = ({ isAllowed, redirectPath, children }) => {
     const user = useAuth()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(false)
+    }, [user])
+
+    if (loading) return null
+
     const allowedUser = isAllowed.includes(user?.user?.role)
 
-    if (!allowedUser) {
-        return <Navigate to={redirectPath} replace />
-    }
+    if (!allowedUser) return <Navigate to={redirectPath} replace />
 
     return children ? children : <Outlet />
 }
