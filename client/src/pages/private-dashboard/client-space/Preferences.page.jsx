12afaf6a4d54d74fb58a1cffbf719422be8c9ed3
Hugo Costier <hugo.costier.development@gmail.com>
2024-04-15 17:53:36 +0200

    Update import path for client preferences in Preferences.page.jsx

--- client/src/pages/private-dashboard/client-space/Preferences.page.jsx
+++ client/src/pages/private-dashboard/client-space/Preferences.page.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import StyledComponents from 'styled-components'
-import { fetchClientPreferences, updateClientPreferences } from '../../../data/admin/preferences'
+import { fetchClientPreferences, updateClientPreferences } from '../../../data/admin/preferences.fetch'
 import { useClientInfo } from '../../../hooks/useClientInfo.hook'
 
 export const MyPreferences = () => {
