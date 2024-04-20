aecb50e4865b9008402e7ce3cf8255c74b38693e
Hugo Costier <hugo.costier.development@gmail.com>
2024-04-20 21:10:29 +0200

    Added new fetchAllAppointments and countAllAppointments methods

--- client/src/data/appointments/appointments.fetch.js
+++ client/src/data/appointments/appointments.fetch.js
@@ -1,6 +1,44 @@
-export const fetchAppointments = (options) => {
-    const API_URL = 'http://localhost:3000/api/v1'
+const API_URL = 'http://localhost:3000/api/v1'
 
+export const fetchAllAppointments = (options) => {
+    const { show, rangeStart, rangeEnd, day } = options
+
+    const body = {
+        ...(show === 'showAll' && { showAll: true }),
+        ...(show === 'showUpcoming' && { showUpcoming: true }),
+        ...(show === 'showHistory' && { showHistory: true }),
+        ...(rangeStart && { rangeStart }),
+        ...(rangeEnd && { rangeEnd }),
+        ...(day && { day }),
+    }
+
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/appointments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(body),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('HTTP Error ! Status: ' + response.status)
+                }
+
+                return response.json()
+            })
+            .then((appointments) => {
+                resolve(appointments)
+            })
+            .catch((error) => {
+                console.error('Error fetching appointments:', error)
+                reject([])
+            })
+    })
+}
+
+export const fetchAppointments = (options) => {
     const { clientId, show, rangeStart, rangeEnd, day } = options
 
     const body = {
@@ -29,9 +67,7 @@ export const fetchAppointments = (options) => {
                 return response.json()
             })
             .then((appointments) => {
-                setTimeout(() => {
-                    resolve(appointments)
-                }, 500)
+                resolve(appointments)
             })
             .catch((error) => {
                 console.error('Error fetching appointments:', error)
@@ -40,9 +76,33 @@ export const fetchAppointments = (options) => {
     })
 }
 
-export const fetchCountAppointments = (options) => {
-    const API_URL = 'http://localhost:3000/api/v1'
+export const fetchAppointment = (appointmentID) => {
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/appointments/${appointmentID}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('HTTP Error ! Status: ' + response.status)
+                }
 
+                return response.json()
+            })
+            .then((appointment) => {
+                resolve(appointment)
+            })
+            .catch((error) => {
+                console.error('Error fetching appointment:', error)
+                reject({})
+            })
+    })
+}
+
+export const countUserAppointments = (options) => {
     const { clientId, firstDayOfWeek, lastDayOfWeek, today } = options
 
     const body = {
@@ -73,6 +133,74 @@ export const fetchCountAppointments = (options) => {
             .catch((error) => {
                 console.error('Error fetching data:', error)
                 reject([])
+            })
+    })
+}
+
+export const countAllAppointments = (date) => {
+    const body = {
+        ...(date && { date }),
+    }
+
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/appointments/count`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(body),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('HTTP Error ! Status: ' + response.status)
+                }
+
+                return response.json()
+            })
+            .then((count) => {
+                resolve(count)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error)
+                reject([])
+            })
+    })
+}
+
+export const updateAppointment = (appointmentID, data) => {
+    const { date, time, status, isAway, privateNotes } = data
+
+    const body = {
+        ...(date && { date }),
+        ...(time && { time }),
+        ...(status && { status }),
+        ...(isAway === 'true' ? { is_away: true } : { is_away: false }),
+        ...(privateNotes && { private_notes: privateNotes }),
+    }
+
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/appointments/${appointmentID}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify(body),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('HTTP Error ! Status: ' + response.status)
+                }
+
+                return response.json()
+            })
+            .then((appointment) => {
+                resolve(appointment)
+            })
+            .catch((error) => {
+                console.error('Error updating appointment:', error)
+                reject({})
             })
     })
 }
