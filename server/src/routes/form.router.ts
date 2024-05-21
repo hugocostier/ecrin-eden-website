10c7c298583be0e36d4ac917f6f7252ff8f6fe9f
Hugo Costier <hugo.costier.development@gmail.com>
2024-05-21 16:55:49 +0200

    Remove unused files

--- server/src/routes/form.router.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import formController from '../controllers/form.controller.js'
-
-const router = express.Router() 
-
-router.post('/send-contact-form', formController.sendContactForm)
-
-export default router
