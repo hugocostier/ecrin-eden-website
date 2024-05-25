c8583e07d8e3cb80d69a4be7bc43b10bdbf0f978
Hugo Costier <hugo.costier.development@gmail.com>
2024-05-26 01:06:05 +0200

    Refactor API URL handling to use environment variables and update the data handling

--- client/src/data/admin/clients.fetch.js
+++ client/src/data/admin/clients.fetch.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/v1/clients'
+const API_URL = `${import.meta.env.VITE_APP_API_URL}/clients`
 
 export const fetchClient = (id) => {
     const url = `${API_URL}/${id}`
@@ -62,16 +62,24 @@ export const fetchClients = () => {
 
 export const addClient = (data) => {
     const formData = new FormData()
-    formData.append('first_name', data.firstName)
-    formData.append('last_name', data.lastName)
-    formData.append('phone_number', data.phone)
-    formData.append('birth_date', data.birthDate)
-    formData.append('address', data.address)
-    formData.append('postal_code', data.postalCode)
-    formData.append('city', data.city)
-    formData.append('profile_picture', data.profilePicture)
-    formData.append('shared_notes', data.sharedNotes)
-    formData.append('private_notes', data.privateNotes)
+    const fields = [
+        { key: 'first_name', value: data.firstName },
+        { key: 'last_name', value: data.lastName },
+        { key: 'phone_number', value: data.phone },
+        { key: 'birth_date', value: data.birthDate === '' ? null : data.birthDate },
+        { key: 'address', value: data.address },
+        { key: 'postal_code', value: data.postalCode },
+        { key: 'city', value: data.city },
+        { key: 'shared_notes', value: data.sharedNotes },
+        { key: 'private_notes', value: data.privateNotes },
+        { key: 'profile_picture', value: data.profilePicture?.length > 0 ? data.profilePicture[0] : undefined },
+    ]
+
+    fields.forEach((field) => {
+        if (field.value !== undefined) {
+            formData.append(field.key, field.value)
+        }
+    })
 
     return new Promise((resolve, reject) => {
         fetch(API_URL, {
@@ -103,16 +111,24 @@ export const updateClient = (id, data) => {
     const url = `${API_URL}/${id}`
 
     const formData = new FormData()
-    formData.append('first_name', data.firstName)
-    formData.append('last_name', data.lastName)
-    formData.append('phone_number', data.phone)
-    formData.append('birth_date', data.birthDate)
-    formData.append('address', data.address)
-    formData.append('postal_code', data.postalCode)
-    formData.append('city', data.city)
-    formData.append('profile_picture', data.profilePicture)
-    formData.append('shared_notes', data.sharedNotes)
-    formData.append('private_notes', data.privateNotes)
+    const fields = [
+        { key: 'first_name', value: data.firstName },
+        { key: 'last_name', value: data.lastName },
+        { key: 'phone_number', value: data.phone },
+        { key: 'birth_date', value: data.birthDate === '' ? null : data.birthDate },
+        { key: 'address', value: data.address },
+        { key: 'postal_code', value: data.postalCode },
+        { key: 'city', value: data.city },
+        { key: 'shared_notes', value: data.sharedNotes },
+        { key: 'private_notes', value: data.privateNotes },
+        { key: 'profile_picture', value: data.profilePicture?.length > 0 ? data.profilePicture[0] : undefined },
+    ]
+
+    fields.forEach((field) => {
+        if (field.value !== undefined) {
+            formData.append(field.key, field.value)
+        }
+    })
 
     return new Promise((resolve, reject) => {
         fetch(url, {
