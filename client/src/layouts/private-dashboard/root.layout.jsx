204425d0872779dd4ba61d4a948e205631e1a8ce
Hugo Costier <hugo.costier.development@gmail.com>
2024-05-30 17:25:21 +0200

    Remove unused comment

--- client/src/layouts/private-dashboard/root.layout.jsx
+++ client/src/layouts/private-dashboard/root.layout.jsx
@@ -44,7 +44,6 @@ const userMenu = [
     {
         name: 'Déconnexion',
         icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M4 18H6V20H18V4H6V6H4V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V18ZM6 11H13V13H6V16L1 12L6 8V11Z"></path></svg>,
-        // icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 10H2V4.00293C2 3.44903 2.45531 3 2.9918 3H21.0082C21.556 3 22 3.43788 22 4.00293V10H21V20.0015C21 20.553 20.5551 21 20.0066 21H3.9934C3.44476 21 3 20.5525 3 20.0015V10ZM19 10H5V19H19V10ZM4 5V8H20V5H4ZM9 12H15V14H9V12Z"></path></svg>
     }
 ]
 
