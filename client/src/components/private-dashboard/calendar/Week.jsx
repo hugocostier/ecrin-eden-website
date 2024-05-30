4babb7ad11ab7d9d17f5a6a59a6571fb7704e282
Hugo Costier <hugo.costier.development@gmail.com>
2024-05-30 17:28:28 +0200

    Update Week component to remove unused prop and improve code readability

--- client/src/components/private-dashboard/calendar/Week.jsx
+++ client/src/components/private-dashboard/calendar/Week.jsx
@@ -1,28 +1,23 @@
-import PropTypes from 'prop-types'
 import StyledComponents from 'styled-components'
+
+export const Week = () => {
+    return (
+        <StyledWeek>
+            <tr>
+                <th>Lun</th>
+                <th>Mar</th>
+                <th>Mer</th>
+                <th>Jeu</th>
+                <th>Ven</th>
+                <th>Sam</th>
+                <th>Dim</th>
+            </tr>
+        </StyledWeek>
+    )
+}
 
 const StyledWeek = StyledComponents.thead`
     th {
         width: calc(100% / 7); 
     }
 `
-
-export const Week = ({ days }) => {
-    return (
-        <StyledWeek>
-            <tr>
-                <th>Lun {days ? days[0] : null}</th>
-                <th>Mar {days ? days[1] : null}</th>
-                <th>Mer {days ? days[2] : null}</th>
-                <th>Jeu {days ? days[3] : null}</th>
-                <th>Ven {days ? days[4] : null}</th>
-                <th>Sam {days ? days[5] : null}</th>
-                <th>Dim {days ? days[6] : null}</th>
-            </tr>
-        </StyledWeek>
-    )
-}
-
-Week.propTypes = {
-    days: PropTypes.array
-}
