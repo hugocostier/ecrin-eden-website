a62ad252595b4968738ab0caabd6288fec6cd0b6
Hugo Costier <hugo.costier.development@gmail.com>
2024-06-11 18:05:31 +0200

    Add search icon to search bar component and update styling

--- client/src/components/private-dashboard/SearchBar.jsx
+++ client/src/components/private-dashboard/SearchBar.jsx
@@ -4,6 +4,9 @@ import StyledComponents from 'styled-components'
 export const SearchBar = ({ searchInput, setSearchParams }) => {
     return (
         <SearchBarContainer className='search-bar'>
+            <i className='search-icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="rgba(24,24,24,1)"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path></svg>
+            </i>
             <input
                 type='text'
                 id='searchInput'
@@ -31,19 +34,30 @@ const SearchBarContainer = StyledComponents.div`
     width: 100%;
     max-width: 400px;
 
+    .search-icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 100%;
+        position: relative;
+        top: 0;
+        left: 0;
+        z-index: 1;
+        background-color: var(--grey-200);
+        border-radius: 8px 0 0 8px;
+        border: 1px solid var(--black);
+    }
+
     input {
         width: 100%;
         height: 100%;
         border: none;
         background-color: var(--grey-200);
-        border-radius: 8px;
+        border-radius: 0 8px 8px 0;
         border: 1px solid var(--black);
-        padding: 0 20px 0 40px;
-        box-shadow: 0 0 0 2px rgb(134 140 160 / 2%);
-        background-image: url('data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 56.966 56.966' fill='%23717790c7'%3e%3cpath d='M55.146 51.887L41.588 37.786A22.926 22.926 0 0046.984 23c0-12.682-10.318-23-23-23s-23 10.318-23 23 10.318 23 23 23c4.761 0 9.298-1.436 13.177-4.162l13.661 14.208c.571.593 1.339.92 2.162.92.779 0 1.518-.297 2.079-.837a3.004 3.004 0 00.083-4.242zM23.984 6c9.374 0 17 7.626 17 17s-7.626 17-17 17-17-7.626-17-17 7.626-17 17-17z'/%3e%3c/svg%3e');
-        background-size: 18px;
-        background-repeat: no-repeat;
-        background-position: 16px 48%;
+        border-left: none;
+        padding: 0 20px 0 10px;
         color: var(--black);
         
         &::placeholder {
