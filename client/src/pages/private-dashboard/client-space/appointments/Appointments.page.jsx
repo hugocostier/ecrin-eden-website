65d88bb1de6fbf132420b464641ef13e35fd5b84
Hugo Costier <hugo.costier.development@gmail.com>
2024-06-18 01:27:54 +0200

    Refactor update button styling for cancelled appointments

--- client/src/pages/private-dashboard/client-space/appointments/Appointments.page.jsx
+++ client/src/pages/private-dashboard/client-space/appointments/Appointments.page.jsx
@@ -103,7 +103,13 @@ export const UserAppointments = () => {
                                         <Link to={`${appointment.id}`} state={{ disable: true }}>
                                             <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'><path d='M12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3ZM12.0003 19C16.2359 19 19.8603 16.052 20.7777 12C19.8603 7.94803 16.2359 5 12.0003 5C7.7646 5 4.14022 7.94803 3.22278 12C4.14022 16.052 7.7646 19 12.0003 19ZM12.0003 16.5C9.51498 16.5 7.50026 14.4853 7.50026 12C7.50026 9.51472 9.51498 7.5 12.0003 7.5C14.4855 7.5 16.5003 9.51472 16.5003 12C16.5003 14.4853 14.4855 16.5 12.0003 16.5ZM12.0003 14.5C13.381 14.5 14.5003 13.3807 14.5003 12C14.5003 10.6193 13.381 9.5 12.0003 9.5C10.6196 9.5 9.50026 10.6193 9.50026 12C9.50026 13.3807 10.6196 14.5 12.0003 14.5Z'></path></svg>
                                         </Link>
-                                        <Link to={`update/${appointment.id}`} state={{ disable: false }}>
+                                        <Link
+                                            to={`update/${appointment.id}`}
+                                            state={{ disable: false }}
+                                            style={
+                                                appointment.status === 'cancelled' ? { pointerEvents: 'none', cursor: 'default', opacity: '0.5' } : null
+                                            }
+                                        >
                                             <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'><path d='M6.41421 15.89L16.5563 5.74785L15.1421 4.33363L5 14.4758V15.89H6.41421ZM7.24264 17.89H3V13.6473L14.435 2.21231C14.8256 1.82179 15.4587 1.82179 15.8492 2.21231L18.6777 5.04074C19.0682 5.43126 19.0682 6.06443 18.6777 6.45495L7.24264 17.89ZM3 19.89H21V21.89H3V19.89Z'></path></svg>
                                         </Link>
                                         <Link
@@ -198,6 +204,7 @@ const AppointmentsTable = StyledComponents.table`
     border-collapse: collapse;
     border-radius: 10px; 
     margin-top: 20px;
+    user-select: none;
 
     thead {
         background-color: #f4f4f4;
