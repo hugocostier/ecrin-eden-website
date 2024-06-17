41f3e4e14d5a245cdf4a41b8b9f4c8ce7db50264
Hugo Costier <hugo.costier.development@gmail.com>
2024-06-18 01:18:38 +0200

    Refactor TimeCard component for appointment reservation form

--- client/src/components/appointment-reservation/TimeCard.jsx
+++ client/src/components/appointment-reservation/TimeCard.jsx
@@ -1,13 +1,77 @@
-export const TimeCard = ({ time, searchParams, setSearchParams, handleSelect }) => {
-    const { id, start } = time
+import PropTypes from 'prop-types'
+import StyledComponents from 'styled-components'
 
-    const handleClick = () => {
-        handleSelect(searchParams.date, id)
-    }
+export const TimeCard = ({ time, watch, register }) => {
+    const isSelected = watch('time') === time
 
     return (
-        <div className='time-card' onClick={handleClick}>
-            <p>{start}</p>
-        </div>
+        <StyledTimeCard
+            className={`time-card ${isSelected ? 'selected' : ''}`}
+            id={time}
+        >
+            <div className='time-container'>
+                <h4>{time}</h4>
+            </div>
+            <input
+                type='radio'
+                name='time'
+                value={time}
+                className='time-radio'
+                {...register('time', { required: 'Veuillez choisir une heure' })}
+            />
+        </StyledTimeCard>
     )
 }
+
+TimeCard.propTypes = {
+    time: PropTypes.string.isRequired,
+    watch: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+}
+
+const StyledTimeCard = StyledComponents.label`
+    display: inline-block;
+    background-color: #e5f9ff; 
+    border-radius: 4px; 
+    margin: 0 6px 12px;
+    padding: 0 16px; 
+    height: 44px; 
+    cursor: pointer; 
+
+    &.selected {
+        background-color: #007ab3; 
+        
+        .time-container {
+            h4 {
+                color: #fff;
+            }   
+        }
+    }
+
+    &:hover {
+        background-color: #ccf2ff; 
+    }
+
+    .time-container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+    
+        h4 {
+            font-family: var(--body-font); 
+            font-size: 1rem;
+            color: #294056;
+            line-height: 1.5rem;
+            margin: 0;  
+        }
+    }
+
+    .time-radio {
+        display: none;
+    }
+  
+    @media screen and (min-width: 1024px) {
+
+    }
+`
