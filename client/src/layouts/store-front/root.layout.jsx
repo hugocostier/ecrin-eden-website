77e0f90ff48377d748a0a028ab2e81a2a65b4c1a
Hugo Costier <hugo.costier.development@gmail.com>
2024-06-20 01:31:45 +0200

    Add Cookie Consent component for displaying cookie policy message and implement Scroll To Top component

--- client/src/layouts/store-front/root.layout.jsx
+++ client/src/layouts/store-front/root.layout.jsx
@@ -1,12 +1,22 @@
-import {
-    Footer,
-    Navbar
-} from '../../components'
-
+import CookieConsent from 'react-cookie-consent'
+import { ScrollToTop } from '../../components/ScrollToTop'
+import { Footer } from '../../components/store-front/Footer'
+import { Navbar } from '../../components/store-front/Navbar'
+``
 export const StoreRoot = () => {
     return (
         <>
+            <ScrollToTop />
             <Navbar />
+            <CookieConsent
+                location='bottom'
+                buttonText='J&apos;accepte'
+                style={{ background: 'var(--primary-500)' }}
+                buttonStyle={{ color: 'var(--black)', fontSize: '14px', backgroundColor: 'var(--secondary-100)' }}
+                expires={150}
+            >
+                Ce site utilise des cookies pour améliorer votre expérience utilisateur. En continuant à naviguer sur ce site, vous acceptez que leur utilisation.
+            </CookieConsent>
             <Footer />
         </>
     )
