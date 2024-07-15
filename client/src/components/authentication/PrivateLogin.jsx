965ca76fa3f56f5d02e9e8c5961fd7e023fd5215
Hugo Costier <hugo.costier.development@gmail.com>
2024-07-15 03:22:47 +0200

    Refactor to check and redirect depending of the value of state.from

--- client/src/components/authentication/PrivateLogin.jsx
+++ client/src/components/authentication/PrivateLogin.jsx
@@ -1,11 +1,13 @@
 import { useContext } from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { RecoveryContext } from '../../context/passwordRecovery.context'
 
 export const PrivateLogin = () => {
     const { accessFromLogin } = useContext(RecoveryContext)
+    const { state } = useLocation()
 
-    if (!accessFromLogin) {
+    if (!accessFromLogin && state?.from !== 'settings') {
+        console.log('redirecting to login')
         return <Navigate to='/login' replace />
     }
 
