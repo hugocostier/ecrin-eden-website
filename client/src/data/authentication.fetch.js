096b4dd8b296819126665d77f97ed9a247b26f37
Hugo Costier <hugo.costier.development@gmail.com>
2024-07-17 19:37:01 +0200

    Add resetPassword and verifyOTP functions

--- client/src/data/authentication.fetch.js
+++ client/src/data/authentication.fetch.js
@@ -54,3 +54,58 @@ export const forgetPassword = async (email, otp) => {
             })
     })
 }
+
+export const resetPassword = async (email, otp, newPassword) => {
+    const cleanEmail = DOMPurify.sanitize(email.trim())
+    const cleanOtp = parseInt(DOMPurify.sanitize(otp))
+    const cleanPassword = DOMPurify.sanitize(newPassword.trim())
+
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/auth/reset-password`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: cleanEmail,
+                otp: cleanOtp,
+                new_password: cleanPassword,
+            }),
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                resolve(data)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error)
+                reject([])
+            })
+    })
+}
+
+export const verifyOTP = async (email, otp) => {
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/auth/verify-otp`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                otp,
+            }),
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                resolve(data)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error)
+                reject(error)
+            })
+    })
+}
