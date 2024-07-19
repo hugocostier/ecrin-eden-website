d0f42511728e17cf5a7200a98fcee94fdb980337
Hugo Costier <hugo.costier.development@gmail.com>
2024-07-19 19:46:16 +0200

    Add updateEmail and updatePassword functionalities

--- client/src/data/authentication.fetch.js
+++ client/src/data/authentication.fetch.js
@@ -109,3 +109,58 @@ export const verifyOTP = async (email, otp) => {
             })
     })
 }
+
+export const updateEmail = async (email, newEmail) => {
+    console.log('email', email)
+    console.log('newEmail', newEmail)
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/auth/update-email`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                new_email: newEmail,
+            }),
+            credentials: 'include',
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                resolve(data)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error)
+                reject(error)
+            })
+    })
+}
+
+export const updatePassword = async (email, currentPassword, newPassword) => {
+    return new Promise((resolve, reject) => {
+        fetch(`${API_URL}/auth/update-password`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                current_password: currentPassword,
+                new_password: newPassword,
+            }),
+            credentials: 'include',
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                resolve(data)
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error)
+                reject(error)
+            })
+    })
+}
